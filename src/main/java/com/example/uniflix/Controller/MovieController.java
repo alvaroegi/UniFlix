9220834c8dfc1f5dc1c2c8c84c01c65191f1379b
raul <raulfaustepolo@gmail.com>
2023-03-24 10:51:47 +0100

    Amend: 24/03

--- src/main/java/com/example/uniflix/Controller/MovieController.java
+++ src/main/java/com/example/uniflix/Controller/MovieController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
