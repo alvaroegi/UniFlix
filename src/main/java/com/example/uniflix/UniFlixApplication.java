8d825e74f94d99f50d5d344ab584c27666affff9
alvaroegi <94553343+alvaroegi@users.noreply.github.com>
2023-03-29 10:45:58 +0200

    29-03

--- src/main/java/com/example/uniflix/UniFlixApplication.java
+++ src/main/java/com/example/uniflix/UniFlixApplication.java
@@ -2,8 +2,10 @@ package com.example.uniflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class UniFlixApplication {
@@ -11,7 +13,16 @@ public class UniFlixApplication {
     public static void main(String[] args) {
         File directorio = new File("C://Uniflix//Peliculas");
         if (!directorio.exists()) {
-            directorio.mkdirs();
+            if(directorio.mkdirs()){
+                File src = new File("src/main/resources/static/images");
+                File dest = new File("C://Uniflix//Peliculas");
+
+                try {
+                    FileSystemUtils.copyRecursively(src, dest);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         SpringApplication.run(UniFlixApplication.class, args);
     }
