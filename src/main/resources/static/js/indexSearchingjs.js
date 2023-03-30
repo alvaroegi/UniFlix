34bed57d82a572e417fd94fa657e6849c04ad59e
alemg29 <alemg2905@gmail.com>
2023-03-30 10:47:07 +0200

    30-03

---
+++ src/main/resources/static/js/indexSearchingjs.js
@@ -0,0 +1,8 @@
+function hide(){
+    let gen = document.getElementById("general");
+    let gen2 = document.getElementById("general2");
+
+    gen.style.display = "none";
+    gen2.style.display = "block";
+
+}
