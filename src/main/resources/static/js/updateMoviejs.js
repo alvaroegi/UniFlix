ed88d19883bd104d942f83dd7f3ec63d1989792d
raul <raulfaustepolo@gmail.com>
2023-04-27 11:14:34 +0200

    Changes by alemg: 24/03

--- src/main/resources/static/js/updateMoviejs.js
+++ src/main/resources/static/js/updateMoviejs.js
@@ -20,3 +20,27 @@ function confirmDelete() {
     else
         confirmed.value = false;
 }
+
+function bold() {
+    var textarea = document.getElementById('synopsis-textarea');
+    var ini = textarea.selectionStart;
+    var fin = textarea.selectionEnd;
+    var newText = "<strong>" + textarea.value.substring(ini, fin) + "</strong>";
+    textarea.value = textarea.value.substring(0, ini) +newText+ textarea.value.substring(fin);
+}
+
+function italic() {
+    var textarea = document.getElementById('synopsis-textarea');
+    var ini = textarea.selectionStart;
+    var fin = textarea.selectionEnd;
+    var newText = "<i>" + textarea.value.substring(ini, fin) + "</i>";
+    textarea.value = textarea.value.substring(0, ini) +newText+ textarea.value.substring(fin);
+}
+
+function underline() {
+    var textarea = document.getElementById('synopsis-textarea');
+    var ini = textarea.selectionStart;
+    var fin = textarea.selectionEnd;
+    var newText = "<u>" + textarea.value.substring(ini, fin) + "</u>";
+    textarea.value = textarea.value.substring(0, ini) +newText+ textarea.value.substring(fin);
+}
